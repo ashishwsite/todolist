8cd7a19bd2765b3b1b80d21441ecf62e3c095d5f
ashish <1508ramashish@gmail.com>
2024-07-05 14:42:47 +0530

    first commit

---
+++ src/MyComponents/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Welcome() {
+  return (
+    <div>
+      <h1 className='container'> Make your todolist Digital</h1>
+      <h5>Add and update todo daily </h5>
+      <h5>view your todolist any time anywhere</h5>
+      <p>todolist always inyour hand</p>
+    </div>
+  )
+}
